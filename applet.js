2b4d76689e1f03626919b269a3b178a084fde563
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2022-03-25 15:23:14 +0100

    Implemented tab-buttons menu
    x

--- applet.js
+++ applet.js
@@ -11,11 +11,5 @@ app.get('/', async (req, res) => {
 // Serve CSS and images as static files from 'public' folder
 app.use(express.static(__dirname + '/public'))
 
-app.use('/resources/', express.static(__dirname + '/images/'));
-
-// app.get('/', function(req, res) {
-    // res.sendFile(path.join(__dirname + '/index.html'))
-// });
-
-// Use port 3000 unless hosted on external service (not NodeJS)
-app.listen(process.env.PORT || 3001, () => console.log(`App now available on http://localhost:3001`));
+// Use port 9001 unless hosted on external service (not NodeJS)
+app.listen(process.env.PORT || 9001, () => console.log(`App now available on http://localhost:9001`));
---
+++ public/js/script.js
@@ -0,0 +1,34 @@
+﻿console.log("Hello, world! We are now in script.js ...");
+
+function setupTabs() {
+    document.querySelectorAll(".tabs__button").forEach(button => {
+        button.addEventListener("click", () => {
+            const sideBar = button.parentElement;
+            const tabsContainer = sideBar.parentElement;
+
+            // Select and button on which the "data" attribute exists, and then pick for "tab"
+            // Example: data-for-tab (using camelCase)...
+            const tabNumber = button.dataset.forTab;
+
+            // Select a specific tab (data-tab="?") using the query button.dataset.forTab stored in tabsNumber
+            const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabNumber}"]`);
+
+            // Remove all active buttons
+            sideBar.querySelectorAll(".tabs__button").forEach(button => {
+                button.classList.remove("tabs__button--active");
+            });
+
+            // Remove all active content (no tab-button pressed)
+            tabsContainer.querySelectorAll(".tabs__content").forEach(tab => {
+                tab.classList.remove("tabs__content--active");
+            });
+
+            button.classList.add("tabs_button--active");
+            tabToActivate.classList.add("tabs__content--active");
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setupTabs();
+});
